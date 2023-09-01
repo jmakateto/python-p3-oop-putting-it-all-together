7a591e3a6da2881fcab2e086fa95057d70c792ae
john makateto <jmakateto@gmail.com>
2023-09-02 00:23:59 +0300

    made changes

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,12 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, author, price, quantity):
+        self.title = title
+        self.author = author
+        self.price = price
+        self.quantity = quantity
+
+    def get_total_price(self):
+        return self.price * self.quantity
+
+    def is_in_stock(self):
+        return self.quantity > 0
